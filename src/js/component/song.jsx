960f4a405778b532c527a0f4b2f59b86536b5ed9
Alejandro Gonzalez <ajgonzale@gmail.com>
2024-07-15 14:36:47 +0000

    clean the code in the song component

--- src/js/component/song.jsx
+++ src/js/component/song.jsx
@@ -3,27 +3,13 @@ import React from "react";
 const Song = ({song, actionsSong, cSong}) => {
 
     function playSong(){
-        
-        //if (cSong.id != null) {
-        //    document.getElementById(cSong.id).pause();
-        //}
-
-        //let audio = document.getElementById(song.id);
-        //audio.src = 'https://playground.4geeks.com'+song.url;
-        //audio.play();
-        /*
-            <audio id={song.id}
-                src={`https://playground.4geeks.com${song.url}`}
-            ></audio>
-         */
-            actionsSong('play', song);
+        actionsSong('play', song);
     }
 
     return (
         <div className="song text-start d-flex" onClick={playSong} >
            <p className="number">{song.id}</p> <p className="name">{song.name}</p>           
         </div>
-
     )
 };
 export default Song;
